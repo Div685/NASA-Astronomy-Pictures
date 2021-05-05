441186dcb066ef756459e41ccd4a53b75e4b0156
Div685 <divyeshpatel685@gmail.com>
2021-05-05 21:41:21 +0300

    Add style for PhotoDetail component

--- src/components/PhotoDesc.js
+++ src/components/PhotoDesc.js
@@ -1,24 +1,30 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../assets/css/PhotoDetail.css';
 
 const PhotoDesc = ({ photo }) => {
   const {
-    title, date, explanation, url,
+    title, date, explanation, url, media_type,
   } = photo;
   return (
-    <div>
-      <p>{title}</p>
-      <p>
-        {' '}
-        {date}
-        {' '}
-      </p>
-      <p>
-        {' '}
-        {explanation}
-        {' '}
-      </p>
-      <img src={url} alt="i" />
+    <div className="photo__detail-main d-flex text-white">
+      <div className="left__side col-5 p-3">
+        <h3 className="h2 text-uppercase font-weight-bold mt-3 mx-0 mb-0 p-0">{title}</h3>
+        <span className="mt-n2">
+          {date}
+        </span>
+        <p className="mt-3">
+          {explanation}
+        </p>
+      </div>
+      <div className="right__side w-100">
+        {
+          media_type === 'video'
+            ? <img src="https://apod.nasa.gov/apod/image/2104/M106-NOAO-HST-1024c.jpg" alt="images" className="w-100" />
+            : <img src={url} alt="i" className="w-100" />
+        }
+      </div>
     </div>
   );
 };
@@ -31,5 +37,6 @@ PhotoDesc.propTypes = {
     date: PropTypes.string.isRequired,
     explanation: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
+    media_type: PropTypes.string.isRequired,
   }).isRequired,
 };
