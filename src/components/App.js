cc01a3f9929419706d5d15552a580b8a131a0f7e
Div685 <divyeshpatel685@gmail.com>
2021-05-05 20:23:20 +0300

    Add style for the Photolist component
    - Created new Css file

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import NavBar from './NavBar';
 import Routes from './Routes';
-import Banner from './Banner';
 import '../assets/css/App.css';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Banner />
       <Routes />
     </div>
   );
--- src/components/PhotoAPOD.js
+++ src/components/PhotoAPOD.js
@@ -2,19 +2,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import '../assets/css/PhotoAPOD.css';
 
 const PhotoAPOD = ({ photo }) => {
   const {
     date, title, url, media_type,
   } = photo;
   return (
-    <Link to={`/photo/${date}`}>
-      <div>
-        { media_type !== 'image'
-          ? <img src="https://apod.nasa.gov/apod/image/2104/M106-NOAO-HST-1024c.jpg" alt="images" />
-          : <img src={url} alt="images" />}
-        <button type="button" className="btn_title">{title}</button>
-        <span>{date}</span>
+    <Link to={`/photo/${date}`} className="">
+      <div className="photo-details">
+        <figure>
+          { media_type !== 'image'
+            ? (
+              <img src="https://apod.nasa.gov/apod/image/2104/M106-NOAO-HST-1024c.jpg" alt="images" />
+            )
+            : (
+              <img src={url} alt="images" />) }
+          <figcaption className="font-weight-bold">{title}</figcaption>
+        </figure>
       </div>
     </Link>
   );
